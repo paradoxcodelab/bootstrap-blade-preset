23442ca721fd98cd20b0cf2ec8af34fa30439310
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2021-01-05 15:37:35 +0800

    Update auth's main layout file

--- templates/resources/views/auth/_layouts/main.blade.php
+++ templates/resources/views/auth/_layouts/main.blade.php
@@ -6,9 +6,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script defer src="{{ asset('js/app.js') }}"></script>
-
     @livewireStyles
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body>
     <div id="app" class="d-flex flex-column min-vh-100">
@@ -27,6 +31,7 @@
         @show
     </div>
 
+    <script src="{{ asset('js/app.js') }}"></script>
     @livewireScripts
 </body>
 </html>
